2ba5550ddea3a6c379082bb0193123e2d0ee3683
Bank-ofa <carlosvelasquezsalazar09@gmail.com>
2025-12-02 20:06:46 -0500

    Add new user Marco Tulio Reyes Molina
    Added new user 'Marco Tulio Reyes Molina' with account details.

--- data.js
+++ data.js
@@ -49,5 +49,21 @@ const usuarios = [
         { descripcion: "Fitz Gerald", fecha: "28/11/2025", monto: "+$500" }
       ]
     }
+  },
+  // NUEVO USUARIO: MARCO TULIO REYES MOLINA (AGREGADO)
+  {
+    email: "marcotulior220@gmail.com",
+    password: "0419",
+    nombre: "MARCO TULIO REYES MOLINA",
+    cuenta: {
+      nombre: "Cuenta Ahorros",
+      numero: "4117 7400 8839 2530",
+      saldo: 2000,
+      creditos: 0,
+      prestamos: 0,
+      movimientos: [
+        { descripcion: "Fitz Gerald law company", fecha: "03/12/2025", monto: "+$2000" }
+      ]
+    }
   }
 ];
